eb06d7ce023e15d7088f121d30314f6fa5838294
aevitas <m4rtijn@gmail.com>
2013-10-02 20:03:26 +0200

    Added Base64 helper and EncryptedCredentials

---
+++ Spectre.Vault/Encryption/Base64.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Spectre.Vault.Encryption
+{
+    /// <summary>
+    /// Base64 helper.
+    /// </summary>
+    internal static class Base64
+    {
+        /// <summary>
+        /// Encodes the specified source to a base64 string.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        internal static string Encode(string source)
+        {
+            return Convert.ToBase64String(Encoding.ASCII.GetBytes(source));
+        }
+
+        /// <summary>
+        /// Decodes the specified source base64 string to an ASCII string.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        internal static string Decode(string source)
+        {
+            var bytes = Convert.FromBase64String(source);
+
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
---
+++ Spectre.Vault/Storage/EncryptedCredentials.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Scylla.XmlEngine;
+
+namespace Spectre.Vault.Storage
+{
+    [XmlElement("EncryptedCredentials")]
+    public class EncryptedCredentials : IEquatable<EncryptedCredentials>
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Username")]
+        public string Username { get; set; }
+
+        [XmlElement("Password")]
+        public string Password { get; set; }
+
+        public EncryptedCredentials(string name, string username, string password)
+        {
+            Name = name;
+            Username = username;
+            Password = password;
+        }
+
+        public EncryptedCredentials()
+        {
+        }
+
+        public override bool Equals(object other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (other.GetType() != this.GetType()) return false;
+            return Equals((EncryptedCredentials) other);
+        }
+
+        public bool Equals(EncryptedCredentials other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name) && string.Equals(Username, other.Username) && string.Equals(Password, other.Password);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Username != null ? Username.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Password != null ? Password.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+}
