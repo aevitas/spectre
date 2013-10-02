04f1836f4283b1dfec14605c61559d3f9904418c
aevitas <m4rtijn@gmail.com>
2013-10-02 20:42:16 +0200

    Enabled logging

--- Spectre/MainWindow.xaml.cs
+++ Spectre/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 using Spectre.Common;
 using Spectre.Vault;
 using Spectre.Vault.Storage;
@@ -18,12 +20,37 @@ namespace Spectre
         {
             InitializeComponent();
 
+            Logging.OnLog += OnLogging;
+
             CredentialManager.OnReloadCompleted += () =>
             {
                 Credentials = new ObservableCollection<EncryptedCredentials>(CredentialManager.GetDisplayEntries());
             };
             
             CredentialManager.Reload();
+
+            Logging.Write("Spectre startup has finished.");
+        }
+
+        private void OnLogging(Logging.LogMessage message)
+        {
+            if (Dispatcher.Thread != Thread.CurrentThread)
+            {
+                // We want those messages displayed asap, because they're already delayed. Thanks!
+                Dispatcher.BeginInvoke(DispatcherPriority.Send, new Logging.LoggingDelegate(OnLogging), message);
+                return;
+            }
+            else
+            {
+                if (RichTextBox.LineCount >= 10000)
+                {
+                    RichTextBox.Clear();
+                    Logging.Write("We've exceeded 10,000 lines - clearing the log window. Log files will be preserved!");
+                }
+
+                RichTextBox.AppendText(message.PlainMessage + Environment.NewLine);
+                RichTextBox.ScrollToEnd();
+            }
         }
 
         private void BtnCopyPassword_Click(object sender, RoutedEventArgs e)
